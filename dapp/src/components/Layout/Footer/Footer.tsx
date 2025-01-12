83d3def2b64ceb53ce5a40d476159b0aea60ddf0
vtiftilov <vlad.tiftilov@yahoo.com>
2025-01-12 21:20:48 +0200

    Update footer links and content for improved clarity and relevance

--- dapp/src/components/Layout/Footer/Footer.tsx
+++ dapp/src/components/Layout/Footer/Footer.tsx
@@ -1,22 +1,15 @@
-import HeartIcon from 'assets/img/heart.svg?react';
-
 export const Footer = () => {
   return (
     <footer className='mx-auto w-full max-w-prose pb-6 pl-6 pr-6 text-center text-gray-400'>
       <div className='flex flex-col items-center text sm text-gray-400'>
         <a
-          className='text-gray-400 text-sm hover:cursor-pointer hover:underline'
-          href='/disclaimer'
-        >
-          Disclaimer
-        </a>
-        <a
           target='_blank'
           className='flex items-center text-sm hover:underline'
-          href='https://multiversx.com/'
+          href='https://github.com/vladtf/election-app-multiversx'
         >
-          Made with <HeartIcon className='mx-1 fill-gray-400' /> by the
-          MultiversX team
+          Election App on MultiversX Blockchain
+          <br />
+          Dapp based on Multiversx Template
         </a>
       </div>
     </footer>
