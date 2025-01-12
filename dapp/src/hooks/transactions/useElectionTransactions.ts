7a21cdcd44abe0604abb154c4c03cd8a8f673bc3
vtiftilov <vlad.tiftilov@yahoo.com>
2025-01-12 12:34:32 +0200

    Rename useSendPingPongTransaction to useElectionTransactions and implement election-related transaction hooks

--- dapp/src/hooks/transactions/useElectionTransactions.ts
+++ dapp/src/hooks/transactions/useElectionTransactions.ts
@@ -4,7 +4,7 @@ import {
   removeAllSignedTransactions,
   removeAllTransactionsToSign
 } from '@multiversx/sdk-dapp/services/transactions/clearTransactions';
-import { contractAddress } from 'config';
+import { API_URL, contractAddress } from 'config';
 import { signAndSendTransactions } from 'helpers/signAndSendTransactions';
 import {
   useGetAccountInfo,
@@ -20,7 +20,8 @@ import {
   PongRawProps
 } from 'types/pingPong.types';
 import { newTransaction } from 'helpers/sdkDappHelpers';
-import { Address } from 'utils/sdkDappCore';
+import { Address, ProxyNetworkProvider } from 'utils/sdkDappCore';
+import { AddressValue, BigIntValue, BigUIntValue, ResultsParser, StringValue, TypedValue } from '@multiversx/sdk-core/out';
 
 type PingPongTransactionProps = {
   type: SessionEnum;
@@ -36,6 +37,12 @@ const PONG_TRANSACTION_INFO = {
   processingMessage: 'Processing Pong transaction',
   errorMessage: 'An error has occured during Pong',
   successMessage: 'Pong transaction successful'
+};
+
+const REGISTER_ELECTION_INFO = {
+  processingMessage: 'Processing Register Election transaction',
+  errorMessage: 'An error has occured during Register Election',
+  successMessage: 'Register Election transaction successful'
 };
 
 export const useSendPingPongTransaction = ({
@@ -197,6 +204,82 @@ export const useSendPingPongTransaction = ({
     []
   );
 
+  const getDisputeIDList = useCallback(
+    async ({ electionId }: any) => {
+      const disputeIdList = await smartContract.methodsExplicit
+        .getDisputeIDList(electionId)
+        .buildQuery();
+
+      const proxyNetworkProvider = new ProxyNetworkProvider(API_URL);
+      let queryResponse = await proxyNetworkProvider.queryContract(disputeIdList);
+      let disputeIdListRes = new ResultsParser().parseQueryResponse(queryResponse, smartContract.getEndpoint('getDisputeIDList'));
+
+      const mappedDisputeIdList = disputeIdListRes.values.map((value: any) => value.items[0].value.toString());
+      return mappedDisputeIdList;
+    }, []
+  );
+
+  const getElectionIdList = useCallback(
+    async () => {
+      const electionIdList = await smartContract.methodsExplicit
+        .getElectionIDList()
+        .buildQuery();
+
+      const proxyNetworkProvider = new ProxyNetworkProvider(API_URL);
+      let queryResponse = await proxyNetworkProvider.queryContract(electionIdList);
+      let electionIdListRes = new ResultsParser().parseQueryResponse(queryResponse, smartContract.getEndpoint('getElectionIDList'));
+
+      const mappedElectionIdList = electionIdListRes.values.map((value: any) => value.items[0].value.toString());
+      return mappedElectionIdList;
+    }, []
+  );
+
+  const getCandidateFee = useCallback(
+    async () => {
+      const candidateFeeQuery = await smartContract.methodsExplicit
+        .getCandidateFee()
+        .buildQuery();
+
+      const proxyNetworkProvider = new ProxyNetworkProvider(API_URL);
+      let queryResponse = await proxyNetworkProvider.queryContract(candidateFeeQuery);
+      let candidateFeeRes = new ResultsParser().parseQueryResponse(queryResponse, smartContract.getEndpoint('getCandidateFee'));
+
+      let candidateFee = candidateFeeRes.firstValue?.valueOf().toString();
+      console.log("Candidate Fee: ", candidateFee);
+
+      return candidateFee;
+    }, []);
+
+
+
+  const registerElection = useCallback(
+    async ({ name, description, start_time, end_time }: any) => {
+      const electionDetails: TypedValue[] = [
+        new StringValue(name),
+        new StringValue(description),
+        new BigIntValue(0),
+        new BigUIntValue(start_time),
+        new BigUIntValue(end_time),
+      ];
+
+      const registerElection = smartContract.methodsExplicit
+        .registerElection(electionDetails)
+        .withSender(new Address(address))
+        .withGasLimit(60000000)
+        .withChainID(getChainId())
+        .buildTransaction();
+
+      const sessionId = await signAndSendTransactions({
+        transactions: [registerElection],
+        callbackRoute: '/dashboard',
+        transactionsDisplayInfo: REGISTER_ELECTION_INFO
+      });
+
+      sessionStorage.setItem(type, sessionId);
+      setPingPongSessionId(sessionId);
+    }, []
+  );
+
   return {
     sendPingTransaction,
     sendPingTransactionFromAbi,
@@ -204,6 +287,10 @@ export const useSendPingPongTransaction = ({
     sendPongTransactionFromAbi,
     sendPingTransactionFromService,
     sendPongTransactionFromService,
+    getElectionIdList,
+    getCandidateFee,
+    getDisputeIDList,
+    registerElection,
     transactionStatus
   };
 };
